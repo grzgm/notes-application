f0025bdac090e613df0361d2a126ed94e903c8ea
Grzegorz <g.malisz@student.fontys.nl>
2022-09-18 20:15:26 +0200

    Razor Pages

---
+++ Notes Application/Notes Application/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Notes_Application.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
