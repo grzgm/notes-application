314ae9dc096d4ac4d79b237798c3aa21c610c3fa
Grzegorz <g.malisz@student.fontys.nl>
2022-09-21 21:32:19 +0200

    Implementing UML

--- Notes Application/Notes Application/Pages/Register.cshtml.cs
+++ Notes Application/Notes Application/Pages/Register.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LogicLayer;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Notes_Application.Pages
 {
     public class RegisterModel : PageModel
     {
+        [BindProperty]
+        public User User { get; set; }
+        public User Email { get; set; }
+        public string mess { get; private set; }
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                mess = "Valid";
+                return Page();
+            }
+            else
+            {
+                mess = "notValid";
+                return Page();
+            }
         }
     }
 }
