c298a27ee00ab5cfdcf495d49242b1bb5033804c
Grzegorz <g.malisz@student.fontys.nl>
2022-10-18 19:52:46 +0200

    Added Unit Test Project

---
+++ Notes Application/UnitTestProject/LoginTest.cs
@@ -0,0 +1,21 @@
+namespace UnitTestProject
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void LoginUser()
+        {
+        }
+
+        [TestMethod]
+        public void LoginUserWithWrongCredentials()
+        {
+        }
+
+        [TestMethod]
+        public void LoginNonExistentUser()
+        {
+        }
+    }
+}
